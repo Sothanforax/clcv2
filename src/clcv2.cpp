0ae93fed7ddf64523263b7d1e9b1c6fc3a47c8fd
Sothanforax <sothanforax@gmail.com>
2021-08-24 10:24:42 -0600

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/clcv2.cpp 	switched BACK to getopt, I'm an ass.

--- src/clcv2.cpp
+++ src/clcv2.cpp
@@ -7,7 +7,7 @@
  * (at your option) any later version.
  */
 #include <iostream>
-#include <argp.h>
+#include <getopt.h>
 #include <ncurses.h>
 #include <string>
 #include <cmath>
@@ -18,34 +18,55 @@ void inttui(){
 }
 
 void intsimple(){
+	bool cnt = true;
+	char opr;
+	double prob[1];
+	double res;
+	while(cnt == true){
+		std::cout << "Simple Prompt v0.1, Escape character is ^C" << std::endl << "Available operators: * / + -" << std::endl;
+		std::cout << ">";
+		std::cin >> prob[0];
+		std::cout << "\n>";
+		std::cin >> opr;
+		std::cout << "\n>";
+		std::cin >> prob[1];
+		switch(opr){
+			case '*': res = prob[0] * prob[1];
+				break;
+			case '/': res = prob[0] / prob[1];	
+				break;
+			case '+': res = prob[0] + prob[1];	
+				break;
+			case '-': res = prob[0] - prob[1];	
+				break;
+			default: std::cout << "\nInvalid operator" << std::endl;
+				break;
+		}
+		std::cout << "\nResult: " << res << std::endl;
+		std::cout << "Exit Prompt? y/n" << std::endl;
+		std::cout << ">";
+		std::cin >> prob[3];
+		if(prob[3] == 'n'){cnt = false;}
+	}
 }
 
 int main(int argc, char *argv[]){
-	std::string arg = argv[1];
-if(argc > 1){
-	if(arg == "--interactive" || "-i"){inttui();}
+	int arguballs;
 
-	if(arg == "--interactive-simple" || "-s"){intsimple();}
 
-	if(arg == "--help" || "-h"){
-		std::cout << "Usage:\n";
-		std::cout << "progname [options] 		Start an interactive prompt\n";
-		std::cout << "progname [options] [expr]	Enter an expression to solve\n";
-		std::cout << "\n	Proof of concept calculator with multiple input methods\n";
-		std::cout << "\nOptions:\n";
-		std::cout << "-i, --interactive		Start a ncurses based interactive tui\n";
-		std::cout << "-s, --interactive-simple	Start a simple interactive prompt\n";
-		std::cout << "-h, --help			Display this help\n";
-		std::cout << "-v, --version			Display version\n" << std::endl;}
-
-	//std::cout << "\nprogname version 0.1" << std::endl;
-
-	//std::cout << "\nやらないか" << std::endl;
-
-	else{std::cout << "Invalid argument!\n progname -h or --help for more." <<std::endl;}
-}
-else{
-	std::cout << "No argument(s) specified" << std::endl;
-}
+	//for reference when writing the getopt(_long) help struct??	
+	/*std::cout << "Usage:\n";
+	std::cout << "progname [options] 		Start an interactive prompt\n";
+	std::cout << "progname [options] [expr]	Enter an expression to solve\n";
+	std::cout << "\n	Proof of concept calculator with multiple input methods\n";
+	std::cout << "\nOptions:\n";
+	std::cout << "-i, --interactive		Start a ncurses based interactive tui\n";
+	std::cout << "-s, --interactive-simple	Start a simple interactive prompt\n";
+	std::cout << "-h, --help			Display this help\n";
+	std::cout << "-v, --version			Display version\n" << std::endl;
+	std::cout << "\nprogname version 0.1" << std::endl;
+	std::cout << "\nやらないか" << std::endl;
+	std::cout << "Invalid argument!\n progname -h or --help for more." <<std::endl;
+	*\std::cout << "No argument(s) specified" << std::endl;
 
 }
